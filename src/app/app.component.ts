c5d599dad6098b781dd352867a1aaef35aa14bce
Mikail Cayoglu <info@mikail-cayoglu.de>
2023-04-23 03:39:18 +0200

    improved build config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -36,8 +36,6 @@ export class AppComponent implements OnInit {
         content: 'I have 5 years of experience in developing technological solutions as Cloud Engineer, Data Engineer and Software Developer. I am dedicated to writing and refactoring clean, reusable, and scalable code in Angular, Node.js, Python and sometimes in C++ while following good practices and development standards.'
       },
     ]);
-
-
     AOS.init();
 
   }
