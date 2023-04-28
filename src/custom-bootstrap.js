92749d84e4064ac3be1abe14f7f34faa97fc485a
Mikail Cayoglu <info@mikail-cayoglu.de>
2023-04-28 03:29:39 +0200

    Revert "added custom-bootstrap.js for optimized usage"
    This reverts commit 5cbd1dba0f70a584d79b1e00811fb0389720b823.

--- src/custom-bootstrap.js
+++
@@ -1,8 +0,0 @@
-import Dropdown from 'bootstrap/js/src/dropdown';
-
-document.addEventListener('DOMContentLoaded', function () {
-  const dropdownElements = [].slice.call(document.querySelectorAll('[data-bs-toggle="dropdown"]'));
-  dropdownElements.map(function (el) {
-    return new Dropdown(el);
-  });
-});
